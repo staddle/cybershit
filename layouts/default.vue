8b665cdf8918ab89e5b90bb89c6aae39d47f84e2
staddle <hirofushikar@gmail.com>
2022-09-24 11:38:54 +0200

    ready for push

--- layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-<template>
-  <div>
-    <HeaderComponent />
-    <Nuxt />
-    <FooterComponent />
-  </div>
-</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,11 +14,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Create responsive quizzes for free with ease. No registration required.' },
+      { hid: 'description', name: 'description', content: 'Create complete quizzes for free with ease. No registration required. Available soon!' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icoGradient.ico' }
     ]
   },
 
@@ -35,11 +35,6 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  pageTransition: {
-    name: 'page',
-    mode: 'out-in'
-  },
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
@@ -48,17 +43,12 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/recaptcha',
     '@nuxtjs/tailwindcss',
     ['nuxt-fontawesome', {
       imports: [
         {
           set: '@fortawesome/free-solid-svg-icons',
           icons: ['faSquareFull', 'faAt', 'faEnvelope', 'faUser']
-        },
-        {
-          set: '@fortawesome/free-brands-svg-icons',
-          icons: ['faGoogle', 'faFacebook']
         }
       ]
     }],
@@ -75,23 +65,11 @@ export default {
           ***REMOVED***
         },
         services: {
-          auth: true,
-          storage: true,
-          database: {
-            emulatorPort: 9000,
-            emulatorHost: 'localhost'
-          },
-          analytics: true
+          database: true
         }
       }
     ]
   ],
-  recaptcha: {
-    hideBadge: true, // Hide badge element (v3 & v2 via size=invisible)
-    mode: 'base', // Mode: 'base', 'enterprise'
-    siteKey: '6LcthQgiAAAAAOMeIqe7sRcEIImZXsSAS51MJ8fS', // Development Site key for requests
-    version: 3 // Version
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
