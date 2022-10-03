d8c84cb770cb9dd232d2195352e2405d92c4a2a8
Niclas Rosteck <hirofushikar@gmail.com>
2022-10-03 20:43:42 +0200

    ChampList works

---
+++ components/League/ChampIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <img :src="icon(champ.name)" :alt="alt(champ.name)" :width="width" :height="height">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import { riotApi } from '~/model/RiotApi'
+import { Champion } from '~/model/Champion'
+
+export default Vue.extend({
+  name: 'ChampIcon',
+  props: {
+    champ: {
+      type: Object as () => Champion,
+      required: true
+    },
+    width: {
+      type: String,
+      default: '50px'
+    },
+    height: {
+      type: String,
+      default: '50px'
+    }
+  },
+  methods: {
+    icon (name : string): string {
+      return riotApi().getChampionImage(name)
+    },
+    alt (name : string): string {
+      return `${name} icon`
+    }
+  }
+})
+</script>
